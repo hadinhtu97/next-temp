d2a58c8c5295ca06cb8f63665fd0cf721254db6e
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-30 03:24:58 +0700

    fix

--- routes/view.js
+++ routes/view.js
@@ -6,11 +6,11 @@ const path = require('path');
 module.exports = (app) => {
 
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname + '/client/build/')));
+        app.use(express.static(path.join(__dirname, '../client/build/')));
 
         app.route('/')
             .get((req, res) => {
-                res.sendFile(path.join(__dirname + '/client/build/index.html'));
+                res.sendFile(path.join(__dirname, '../client/build/index.html'));
             });
     } else {
         app.route('/')
