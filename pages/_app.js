63203631202b4c6560c979a7b24b2071bdd290ec
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 00:09:41 +0700

    add progress bar to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import NextNprogress from 'nextjs-progressbar'
 
-const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <NextNprogress color='#ffa500' />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
-export default App
+export default MyApp
